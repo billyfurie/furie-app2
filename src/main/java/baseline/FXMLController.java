c279caba8e01cc6dc478636129b8f17fd51e2885
BillyFurie <81727683+BillyFurie@users.noreply.github.com>
2021-11-11 18:41:26 -0500

    First commit. Includes the GUI mockup for the app.

---
+++ src/main/java/baseline/FXMLController.java
@@ -0,0 +1,19 @@
+/*
+ *  UCF COP3330 Fall 2021 Application Assignment 2 Solution
+ *  Copyright 2021 William Furie
+ */
+
+package baseline;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class FXMLController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
+
---
+++ src/main/java/baseline/InventoryManagementApplication.java
@@ -0,0 +1,34 @@
+/*
+ *  UCF COP3330 Fall 2021 Application Assignment 2 Solution
+ *  Copyright 2021 William Furie
+ */
+
+package baseline;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class InventoryManagementApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+
+        stage.setTitle("Inventory Manager");
+        stage.setScene(scene);
+
+        // min size
+        stage.setMinHeight(450);
+        stage.setMinWidth(640);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
